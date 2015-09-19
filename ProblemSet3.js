e05527e8a3e8cd6f48ea1f04ec1a59c78170ec1d
Student <student@Mac-mini-31.local>
2015-09-19 11:06:19 -0600

    new set of problems

---
+++ ProblemSet3.js
@@ -0,0 +1,29 @@
+var palindrome = function (str) {
+    for (var i=0; i<(str.length/2); i++){
+        if (str[i]!==str[str.length-1-i])
+            return false
+        else
+            return true
+    }
+    
+}
+
+
+var dashInsert = function (num) {
+    var m=0;
+    var arr = num.toString().split("")
+    var arr2 = num.toString().split("")
+    for (var i=0; i<(arr.length-1); i++){
+        if ((Number(arr[i])%2)!==0 && (Number(arr[i+1])%2)!==0){
+            arr2.splice(m+1, 0,'-')
+            m++
+        }
+        m++
+    } 
+        
+        return arr2.join("");
+    
+}
+
+
+dashInsert(454793);
